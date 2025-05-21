458ae82b3da605bd9123abcb9ef2793e68d34ef4
jeisson88 <jeisson888567.lo@gmail.com>
2025-05-21 12:34:59 -0600

    feat(backend): Agregar campo y API para proveedoren productos

--- dicaprios_backend/productos/admin.py
+++ dicaprios_backend/productos/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Producto, Categoria, Proveedor
 
-# Register your models here.
+admin.site.register(Producto)
+admin.site.register(Categoria)
+admin.site.register(Proveedor)
--- dicaprios_backend/productos/views.py
+++ dicaprios_backend/productos/views.py
@@ -1,20 +1,17 @@
 from rest_framework import viewsets
-from .models import Producto
-from .models import Categoria
-from .models import Proveedor
-from .serializers import ProductoSerializer
-from .serializers import CategoriaSerializer
-from .serializers import ProveedorSerializer
-
-class ProductoViewSet(viewsets.ModelViewSet):
-    queryset = Producto.objects.all()
-    serializer_class = ProductoSerializer
+from .models import Producto, Categoria, Proveedor # Importar Proveedor
+from .serializers import ProductoSerializer, CategoriaSerializer, ProveedorSerializer # Importar ProveedorSerializer
 
 class CategoriaViewSet(viewsets.ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
 
+class ProductoViewSet(viewsets.ModelViewSet):
+    queryset = Producto.objects.all()
+    serializer_class = ProductoSerializer
+
+# --- NUEVO O ASEGURAR QUE EXISTE ---
 class ProveedorViewSet(viewsets.ModelViewSet):
     queryset = Proveedor.objects.all()
     serializer_class = ProveedorSerializer
-
+# --- FIN NUEVO ---
